4f08fc350e27144adfe00f9929974606fd00d386
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-03 15:42:56 +0200

    added  productlist page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Home />;
+  return <ProductList />;
 };
 
 export default App;
