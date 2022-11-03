9f7512e840011b094920a4b3caa70eb0623a8c65
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-03 14:01:49 +0200

    Home page components added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
+import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
 import Slider from "../components/Slider";
 
@@ -12,6 +14,8 @@ const Home = () => {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter />
+      <Footer />
     </div>
   );
 };
