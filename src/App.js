22ce5ff1ffdd21eb1eb85acf13fae16ed6ac4503
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-03 16:25:21 +0200

    register login and cart page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <ProductList />;
+  return <Cart />;
 };
 
 export default App;
