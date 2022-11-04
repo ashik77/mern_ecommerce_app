62c1407020b0f0a87580be2b4b387f1b238d9845
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-04 14:56:43 +0200

    login api added

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -1,16 +1,58 @@
 import { User } from "../models/user.model.js";
+import CryptoJS from "crypto-js";
+import jwt from "jsonwebtoken";
 
 //USER REGISTRATION
 export const register = async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SECRET
+    ).toString(),
   });
 
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+//USER LOGIN
+
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      userName: req.body.user_name,
+    });
+
+    !user && res.status(401).json("Wrong User Name");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SECRET
+    );
+
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    const inputPassword = req.body.password;
+
+    originalPassword != inputPassword && res.status(401).json("Wrong Password");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
--- api/src/routers/auth.router.js
+++ api/src/routers/auth.router.js
@@ -1,10 +1,12 @@
 import express from "express";
 
-import { register } from "../controllers/auth.controller.js";
+import { login, register } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+
+router.post("/login", login);
 
 // router.post("/users/signin", signIn);
 
