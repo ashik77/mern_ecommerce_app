e55760e8c0251d70822688f42165fa0936ebaf09
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-04 10:48:03 +0200

    server and mongoDB connection

---
+++ api/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// import userRoute from "./src/routers/user.router.js";
+// import memoryRoute from "./src/routers/memory.router.js";
+
+dotenv.config({ path: ".env" });
+
+const app = express();
+
+app.set("port", process.env.PORT);
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json({ extended: true }));
+// app.use(cors());
+
+// //app.use("/api/v1", postRoute);
+// app.use("/api/v1", userRoute);
+// app.use("/api/v1/memory", memoryRoute);
+
+export default app;
