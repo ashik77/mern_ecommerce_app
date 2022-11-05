2d4b15eb2aebecd23eab682a338680ff93361e5c
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-05 18:25:24 +0200

    router implemented

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,35 @@ import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  redirect,
+} from "react-router-dom";
+
 const App = () => {
-  return <Home />;
+  const loggedUser = false;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/products/:category" element={<ProductList />} />
+
+        <Route path="/product/:id" element={<Product />} />
+
+        <Route path="/cart" element={<Cart />} />
+
+        <Route
+          path="/register"
+          element={loggedUser ? <Home /> : <Register />}
+        />
+
+        <Route path="/login" element={loggedUser ? <Home /> : <Login />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
